fb2a665254e1b987dee1e671b1a4ac1f822273ec
Maggie DeVito <maggiedevito@Maggies-iPhone.hitronhub.home>
2021-02-16 13:24:02 -0800

    created a interviewerListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  let interviewerListItemClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+  return (
+    <li
+      onClick={() => props.setInterviewer(props.name)}
+      className={interviewerListItemClass}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
