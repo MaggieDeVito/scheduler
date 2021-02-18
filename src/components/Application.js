3d1c274cec2a067b5d9526f15a03ee0d63e558d5
Maggie DeVito <maggiedevito@Maggies-iPhone.hitronhub.home>
2021-02-17 20:47:27 -0800

    updated our states

--- src/components/Application.js
+++ src/components/Application.js
@@ -52,8 +52,13 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+  });
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get(`/api/days`).then((response) => {
@@ -71,7 +76,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
